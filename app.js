6751d83919045458356ce682ad6ff77b4ae99d50
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-09-03 01:05:46 +0600

    author image added

--- app.js
+++ app.js
@@ -31,39 +31,39 @@ const newsDetails = async () => {
     displayNewsDeteils(data.data);
 }
 
-// const displayNewsDeteils = async (newses) => {
-//     const cardDetails = document.getElementById('card-details');
-//     for (news of newses) {
-//         const div = document.createElement('div');
-//         div.classList.add('container');
-//         div.innerHTML = `
-//         <div class="row g-0 mt-3">
-//         <div class="col-md-4">
-//           <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
-//         </div>
-//         <div class="col-md-8">
-//           <div class="card-body">
-//             <h5 class="card-title p-3">${news.title}</h5>
-//             <p class="card-text p-3">${news.details.length > 300 ? news.details.slice(0, 300) + '...' : news.deteils}</p>
-//             <div class="d-flex justify-content-between">
-//         <div class="d-flex">
-//             <img class="img-fluid img-thumbnail h-25 w-25" src="${news.thumbnail_url}" alt="">
-//             <h6></h6>
-//         </div>
-//         <div class="d-flex">
-//             <h3>view</h3>
-//             <button>Details</button>
-//         </div>
-//     </div>
-//           </div>
-//         </div>
-//       </div>
-//         `;
-//         cardDetails.appendChild(div);
-//         console.log(news.thumbnail_url);
-//     }
+const displayNewsDeteils = async (newses) => {
+    const cardDetails = document.getElementById('card-details');
+    for (news of newses) {
+        const div = document.createElement('div');
+        div.classList.add('container');
+        div.innerHTML = `
+        <div class="row g-0 mt-3">
+        <div class="col-md-4">
+          <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title p-3">${news.title}</h5>
+            <p class="card-text p-3">${news.details.length > 300 ? news.details.slice(0, 300) + '...' : news.deteils}</p>
+            <div class="d-flex justify-content-between">
+        <div class="d-flex">
+            <img class="img-fluid img-thumbnail h-25 w-25" src="${news.thumbnail_url}" alt="">
+            <h6></h6>
+        </div>
+        <div class="d-flex">
+            <h3>view</h3>
+            <button>Details</button>
+        </div>
+    </div>
+          </div>
+        </div>
+      </div>
+        `;
+        cardDetails.appendChild(div);
+        console.log(news.thumbnail_url);
+    }
 
-// }
+}
 newsDetails();
 
 
