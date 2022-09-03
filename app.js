201f5bb3cd44167d06e23020c5780f1a7598e9a5
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-09-03 14:51:56 +0600

    all news added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const cetagoryItem = async () => {
   const url = 'https://openapi.programming-hero.com/api/news/categories';
   const res = await fetch(url);
   const data = await res.json();
+  // console.log(data.data);
   displayCetagory(data.data.news_category);
 }
 const displayCetagory = async (cetagorys) => {
@@ -9,13 +10,16 @@ const displayCetagory = async (cetagorys) => {
   // const ul = document.createElement('ul');
 
   for (cetagory of cetagorys) {
+    console.log(cetagory.category_id);
+    const idCetagory = cetagory.category_id;
     const ul = document.createElement('ul');
     ul.classList.add('navbar-nav')
     ul.innerHTML = `
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">${cetagory.category_name}</a>
+          <a onclick="cetagoryId('${idCetagory}')" class="nav-link active" aria-current="page" href="#">${cetagory.category_name}</a>
         </li>
         `;
+
     cetagoryName.appendChild(ul);
     // console.log(cetagory.category_name);
   };
@@ -24,15 +28,39 @@ const displayCetagory = async (cetagorys) => {
 cetagoryItem();
 
 
-const newsDetails = async () => {
-  const url = 'https://openapi.programming-hero.com/api/news/category/01';
+const cetagoryId = async (category_id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
   const res = await fetch(url);
   const data = await res.json();
-  displayNewsDeteils(data.data);
+  displayCatagoryId(data.data);
+}
+
+const displayCatagoryId = async (cetagorysID) => {
+  console.log(cetagorysID);
+  for (allCetagory of cetagorysID) {
+    console.log(allCetagory.category_id);
+    // console.log(allCetagory.category_id);
+    const url = `https://openapi.programming-hero.com/api/news/category/${allCetagory.category_id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    displayNewsDeteils(data.data);
+  }
+  // return allCetagory.category_id;
+}
+
+
+const newsDetails = async (cetagorysID) => {
+  // const url = `https://openapi.programming-hero.com/api/news/category/${}`;
+  // const res = await fetch(url);
+  // const data = await res.json();
+  // console.log(data);
+  // displayNewsDeteils(data.data);
 }
 
 const displayNewsDeteils = async (newses) => {
   const cardDetails = document.getElementById('card-details');
+  cardDetails.textContent = "";
   for (news of newses) {
     const div = document.createElement('div');
     div.classList.add('container');
@@ -46,7 +74,7 @@ const displayNewsDeteils = async (newses) => {
             <h5 class="card-title p-3">${news.title}</h5>
             <p class="card-text p-3">${news.details.length > 300 ? news.details.slice(0, 300) + '...' : news.deteils}</p>
             <div class="d-flex justify-content-between p-3">
-           
+            <img class="img-tumbnail" src="" alt="">
             <span>${news.author.name}</span>
             <span>${news.total_view}</span>
             <button type="button" class="btn btn-primary">Primary</button>
@@ -61,11 +89,13 @@ const displayNewsDeteils = async (newses) => {
       </div>
         `;
     cardDetails.appendChild(div);
-    console.log(news.total_view);
+
+    // console.log(news.category_id);
   }
 
 }
 newsDetails();
+// displayCatagoryId();
 
 
 
